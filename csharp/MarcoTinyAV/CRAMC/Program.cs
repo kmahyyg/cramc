81a773add83adda11268e422c95e43a33e7e890b
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2025-05-28 18:48:16 +0800

    update gitignore and init csharp framework

---
+++ csharp/MarcoTinyAV/CRAMC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CRAMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
