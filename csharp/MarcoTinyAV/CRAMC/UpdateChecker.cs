d6eaac830fbbc21138ff339059ba73667cc7ba57
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-05-29 15:27:16 +0800

    save work progress

--- csharp/MarcoTinyAV/CRAMC/UpdateChecker.cs
+++ csharp/MarcoTinyAV/CRAMC/UpdateChecker.cs
@@ -1,5 +1,39 @@
+using System;
+using Serilog;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace CRAMC;
 
-public class UpdateChecker {
+internal class UpdateChecker {
+    private static string _versionCheckUrl =
+        "https://cdn.jsdelivr.net/gh/kmahyyg/cramc@master/assets/latest_version.json";
+
+    private static int _currentProgramRev = 1;
+
+    public class LatestVersion {
+        [JsonProperty]
+        public int databaseVersion { get; set; }
+        [JsonProperty]
+        public int programRevision { get; set; }
+    }
+
+    private LatestVersion? _latestVersion;
+
+    public async Task<LatestVersion> GetLatestVersionFromGitHub() {
+    }
     
+    public int CheckProgramUpdates() {
+        if (_latestVersion == null) {
+
+        } else {
+            return _latestVersion.databaseVersion;
+        }
+    }
+
+    public int CheckDatabaseUpdates() {
+        
+    }
 }
