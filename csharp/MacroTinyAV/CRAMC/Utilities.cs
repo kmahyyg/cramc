4e95616f14b64f3737f1297c8c2a87d2719e77cf
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-05-30 15:04:48 +0800

    recollect files

--- csharp/MacroTinyAV/CRAMC/Utilities.cs
+++ csharp/MacroTinyAV/CRAMC/Utilities.cs
@@ -15,14 +15,19 @@ public static class Utilities {
         // file on-demand, size on disk == 0, and with:
         // Windows File Attr: O FILE_ATTRIBUTE_OFFLINE (0x00001000)
         // Windows File Attr: U FILE_ATTRIBUTE_UNPINNED (0x00100000)
-        // Windows File Attr: LM FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS (0x00400000)
+        // https://ss64.com/nt/attrib.html 
+        // Windows File Attr: M FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS (0x00400000)
         //
         // for macos/linux:
         // directly return file.exists and fileinfo.size
         if (File.Exists(path)) {
-            if (RuntimeOpts.IsWindows)
-            {
-                
+            if (RuntimeOpts.IsWindows) {
+                var fAttr = File.GetAttributes(path);
+                if (fAttr.HasFlag(FILE_ATTRIBUTE_UNPINNED) || fAttr.HasFlag(FileAttributes.Offline) ||
+                    fAttr.HasFlag(FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS)) {
+                    // file stored on remote disk, not available on local
+                    return -1;
+                }
             }
             else {
                 Log.Warning("Detected non-Windows environment, returned size is logical size, not size on disk.");
