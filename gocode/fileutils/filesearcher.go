4fa97e7567176cee7335330cd5edf6f4a1ca6541
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2025-06-01 18:57:51 +0800

    finish general walkthrough searcher

--- gocode/fileutils/filesearcher.go
+++ gocode/fileutils/filesearcher.go
@@ -1,1 +1,43 @@
 package fileutils
+
+import (
+	"cramc_go/common"
+	"io/fs"
+	"os"
+	"path"
+	"slices"
+	"strings"
+)
+
+func GeneralWalkthroughSearch(actionPath string, allowedExts []string, outputChan chan string) (int, error) {
+	defer close(outputChan)
+	fsRoot := os.DirFS(actionPath)
+	fsRootDir := strings.ReplaceAll(actionPath, "\\", "/")
+
+	counter := 0
+	walkFn := func(curPath string, d fs.DirEntry, err error) error {
+		// filter1: prefix path already in place
+		// filter2: allowedExts
+		if err != nil {
+			// safely ignore errors as you can't access these file under current privilege
+			// neither virus nor you can access
+			common.Logger.Warningln(err.Error())
+			return nil
+		}
+		if d.IsDir() {
+			return nil
+		}
+		fExt := path.Ext(curPath)
+		if slices.Contains(allowedExts, fExt) {
+			counter += 1
+			outputChan <- fsRootDir + "/" + curPath
+		}
+		return nil
+	}
+
+	err := fs.WalkDir(fsRoot, ".", walkFn)
+	if err != nil {
+		return -1, err
+	}
+	return counter, nil
+}
