2ad2376da78cc7dddeeef4ae8e1cc4f046709d3f
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2025-06-01 11:50:24 +0800

    add judgement of ntfs

--- gocode/fileutils/filesearcher_booster_windows.go
+++ gocode/fileutils/filesearcher_booster_windows.go
@@ -18,7 +18,7 @@ func CheckProcessElevated() (bool, error) {
 	var curProcTokenR windows.Token
 	err = windows.OpenProcessToken(windows.CurrentProcess(), windows.TOKEN_QUERY, &curProcTokenR)
 	if err != nil {
-		log.Fatalln(err)
+		common.Logger.Fatalln(err)
 	}
 	defer curProcTokenR.Close()
 	if curProcTokenR.IsElevated() {
@@ -26,14 +26,46 @@ func CheckProcessElevated() (bool, error) {
 	} else {
 		return false, customerrs.ErrInsufficientPrivilege
 	}
-	return false, customerrs.ErrUnknownInternalError
 }
 
 func IsDriveFileSystemNTFS(actionPath string) (bool, error) {
+	// Extract drive letter from the first character
+	driveLetter := actionPath[0]
 
-	return false, customerrs.ErrUnknownInternalError
+	// Construct root path (e.g., "C:\")
+	rootPath := string(driveLetter) + ":\\"
+
+	// Convert to UTF16 for Windows API
+	rootPathPtr, err := windows.UTF16PtrFromString(rootPath)
+	if err != nil {
+		return false, err
+	}
+
+	// Buffer for filesystem name (32 characters should be enough for filesystem names)
+	fileSystemNameBuffer := make([]uint16, 32)
+
+	// Call GetVolumeInformation to get filesystem name
+	err = windows.GetVolumeInformation(
+		rootPathPtr,                       // lpRootPathName
+		nil,                               // lpVolumeNameBuffer (we don't need volume name)
+		0,                                 // nVolumeNameSize
+		nil,                               // lpVolumeSerialNumber
+		nil,                               // lpMaximumComponentLength
+		nil,                               // lpFileSystemFlags
+		&fileSystemNameBuffer[0],          // lpFileSystemNameBuffer
+		uint32(len(fileSystemNameBuffer)), // nFileSystemNameSize
+	)
+
+	if err != nil {
+		return false, err
+	}
+
+	// Convert UTF16 buffer to string
+	fileSystemName := windows.UTF16ToString(fileSystemNameBuffer)
+
+	return fileSystemName == "NTFS", customerrs.ErrUnknownInternalError
 }
 
 func ExtractAndParseMFT(actionPath string, allowedExts []string, outputChan chan string) (int64, error) {
-	return false, customerrs.ErrUnknownInternalError
+	return -1, customerrs.ErrUnknownInternalError
 }
