55f741536322721d61dbfa64415735f94a147ff2
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-06-02 18:45:29 +0800

    finish devreleaser

--- gocode/cmd/devreleaser/main.go
+++ gocode/cmd/devreleaser/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"cramc_go/common"
+	"cramc_go/cryptutils"
+	"cramc_go/customerrs"
 	"cramc_go/logging"
 	"cramc_go/yara_scanner"
+	"encoding/hex"
 	"flag"
 	"os"
 )
 
 var (
-	fEnc  = flag.Bool("enc", false, "encrypt")
-	fComp = flag.Bool("compile", false, "compile yara rules")
-	fDec  = flag.Bool("dec", false, "decrypt")
+	fEnc     = flag.Bool("enc", false, "encrypt")
+	fComp    = flag.Bool("compile", false, "compile yara rules")
+	fDec     = flag.Bool("dec", false, "decrypt")
+	fInFile  = flag.String("in", "unset-placeholder", "input file")
+	fOutFile = flag.String("out", "unset-placeholder", "output file")
 )
 
 const (
@@ -42,10 +47,54 @@ func main() {
 		common.Logger.Infoln("Operation finished.")
 		return
 	}
-	if *fDec {
-
+	if *fDec && *fEnc {
+		panic(customerrs.ErrInvalidInput)
 	}
-	if *fEnc {
-
+	if *fDec || *fEnc {
+		if !checkFileLogicalExists(*fInFile) {
+			panic(customerrs.ErrInvalidInput)
+		}
+		inData, err := os.ReadFile(*fInFile)
+		if err != nil {
+			panic(err)
+		}
+		outFd, err := os.Create(*fOutFile)
+		if err != nil {
+			panic(err)
+		}
+		defer outFd.Close()
+		passwd, err := hex.DecodeString(common.HexEncryptionPassword)
+		if err != nil {
+			panic(err)
+		}
+		var outD []byte
+		if *fEnc {
+			outD, err = cryptutils.XChacha20Encrypt(passwd, inData)
+			if err != nil {
+				panic(err)
+			}
+		} else if *fDec {
+			outD, err = cryptutils.XChacha20Decrypt(passwd, inData)
+			if err != nil {
+				panic(err)
+			}
+		}
+		_, err = outFd.Write(outD)
+		if err != nil {
+			panic(err)
+		}
+		outFd.Sync()
+		return
 	}
+}
+
+func checkFileLogicalExists(filename string) bool {
+	if len(filename) == 0 {
+		return false
+	}
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
 }
