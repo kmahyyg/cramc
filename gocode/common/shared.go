760e6ae99318728c70b9f65fbe4ae21b347f8160
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-06-23 15:41:47 +0800

    bump version for incompatible update

--- gocode/common/shared.go
+++ gocode/common/shared.go
@@ -26,5 +26,5 @@ var (
 const (
 	// it's insecure, don't hardcode any password, but i'm lazy, so here, it's intended.
 	HexEncryptionPassword = "1928da3545b48068e024d06f2f132c728eabcd933a8659e578d7a82fde0cd948"
-	ProgramRev            = 8
+	ProgramRev            = 9
 )
