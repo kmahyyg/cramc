269715d2c57a296d8781cc78589414d4c8be3c91
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-06-23 23:42:49 +0800

    add comment for yr fmt

--- assets/pre-commit-hooks.sh
+++ assets/pre-commit-hooks.sh
@@ -5,5 +5,6 @@ if ! which yr >/dev/null 2>&1; then
   exit 1;
 fi
 
+# https://virustotal.github.io/yara-x/blog/the-fmt-command/
 find assets/yrules/ -type f -iname '*.yar' -exec bash -c 'yr fmt "{}"' \;
 exit 0
