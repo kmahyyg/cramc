9df0ebfe9d64f86310ac161c41b6fa5467d692c2
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2026-03-21 14:39:10 +0800

    bump version and remove unused functions

--- cramc_go/common/datamodels.go
+++ cramc_go/common/datamodels.go
@@ -25,18 +25,35 @@ type SingleHardenMeasure struct {
 type YaraScanResult struct {
 	DetectedRule string `json:"detectedRule"`
 	FilePath     string `json:"filePath"`
+	Module       string `json:"module"`
 }
 
-// IPC, request to sanitize
-type IPCSingleDocToBeSanitized struct {
+// request to sanitize
+type SanitizeRequest struct {
 	Path          string `json:"path"`
 	Action        string `json:"action"`
 	DetectionName string `json:"detectionName"`
 	DestModule    string `json:"module"`
 
 	// additional optional field for grpc
-	MessageID uint64 `json:"messageID,omitempty"`
+	MessageID uint64 `json:"msgID,omitempty"`
 }
+
+type SanitizeResult struct {
+	RequestMsgID uint64 `json:"reqMsgID,omitempty"`
+	Path         string `json:"path,omitempty"`
+	Success      bool   `json:"success,omitempty"`
+	Message      string `json:"message,omitempty"`
+	Error        error  `json:"error,omitempty"`
+}
+
+type ExtractedVBAModule struct {
+	ModuleName string `json:"moduleName,omitempty"`
+	SourceCode []byte `json:"sourceCode,omitempty"`
+	TextOffset uint32 `json:"-"`
+}
+
+type ExtractedVBAModules = []*ExtractedVBAModule
 
 type HardeningAction struct {
 	Name                string                 `json:"name"`
--- cramc_go/common/shared.go
+++ cramc_go/common/shared.go
@@ -14,12 +14,12 @@ var (
 	IsRunningOnWin = runtime.GOOS == "windows"
 	IsElevated     bool
 
-	VersionStr    string
-	SanitizeQueue = make(chan *IPCSingleDocToBeSanitized, 100)
+	VersionStr     string
+	SanitizerQueue = make(chan *SanitizeRequest, 50)
 
 	DryRunOnly                 bool
 	EnableHardening            bool
-	HardeningQueue             = make(chan *HardeningAction, 100)
+	HardeningQueue             = make(chan *HardeningAction, 50)
 	HardenedDetectionTypesLock = &sync.Mutex{}
 	HardenedDetectionTypes     = make(map[string]bool)
 )
@@ -27,5 +27,5 @@ var (
 const (
 	// it's insecure, don't hardcode any password, but i'm lazy, so here, it's intended.
 	HexEncryptionPassword = "1928da3545b48068e024d06f2f132c728eabcd933a8659e578d7a82fde0cd948"
-	ProgramRev            = 16
+	ProgramRev            = 17
 )
